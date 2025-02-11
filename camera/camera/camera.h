822b504207022175505834cd23d9e4ae93d37fbc
SamFromCam <samnangnuonsineoun2025@u.northwestern.edu>
2025-02-10 18:42:32 -0800

    add folder

---
+++ camera/camera/camera.h
@@ -0,0 +1,32 @@
+/*
+ * camera.h
+ *
+ * Created: 2/10/2025 5:37:35 PM
+ *  Author: SamFromCam Window
+ */ 
+
+
+#ifndef CAMERA_H_
+#define CAMERA_H_
+#define D0 PIO_PA24
+#define D1 PIO_PA25
+#define	D2 PIO_PA26
+#define D3 PIO_PA27
+#define D4 PIO_PA28
+#define D5 PIO_PA29
+#define D6 PIO_PA30
+#define D7 PIO_PA31
+#define HREF PIO_PA16
+#define RST PIO_PA20
+#define PCLK PIO_PA23
+#define XCLK PIO_PA17
+#define SCL PIO_PA4
+#define VSYND PIO_PA15
+#define SDA PIO_PA3
+
+
+
+
+
+
+#endif /* CAMERA_H_ */
